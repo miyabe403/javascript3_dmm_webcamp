6212d946fa7f3affe35b968f3cd458ceac94e56e
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-03 19:54:27 +0900

    [Add] 「じゃんけんプログラム」に、機能を追加

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ alert(promptStr);
 // じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
+// グー・チョキ・パー以外の文字列が入力されたときに、「グー・チョキ・パーのいずれかを入力してください」とアラートウィンドウにエラーが表示されるように設定。
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  // アラートウィンドウでエラーが表示された後、もう一度、プロンプト入力欄が表示されて入力できるように設定。
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
+
 // じゃんけんの手をランダムに作成する関数を呼び出す
 let js_hand = getJShand();
 
@@ -29,7 +36,12 @@ let js_hand = getJShand();
 let judge = winLose(user_hand, js_hand);
 
 // 結果を表示する
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  // キャンセルボタンが押されたときは、「またチャレンジしてね」というアラートメッセージが出力されるように設定。
+  alert("またチャレンジしてね")
+}
 
 // ランダムでじゃんけんの手を作成する関数
 function getJShand(){
